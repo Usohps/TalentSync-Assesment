0f692bd64fd10f5ee7087703a6beda1c1d45fcc4
Usohps <usohps@gmail.com>
2024-02-01 02:55:56 +0100

    Accordion update

---
+++ src/app/accordion_data/DummyData.jsx
@@ -0,0 +1,34 @@
+export const dummyData = {
+  datalist: [
+    {
+      id: 1,
+      title: "How many participants can join a ClearLink video conference?",
+      content:"ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scalewebinars, ensuring you have the right fit for your needs."
+    },
+    {
+        id: 2,
+        title: "How many participants can join a ClearLink video conference?",
+        content:"ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scalewebinars, ensuring you have the right fit for your needs."
+      },
+      {
+        id: 3,
+        title: "How many participants can join a ClearLink video conference?",
+        content:"ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scalewebinars, ensuring you have the right fit for your needs."
+      },
+      {
+        id: 4,
+        title: "How many participants can join a ClearLink video conference?",
+        content:"ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scalewebinars, ensuring you have the right fit for your needs."
+      },
+      {
+        id: 5,
+        title: "How many participants can join a ClearLink video conference?",
+        content:"ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scalewebinars, ensuring you have the right fit for your needs."
+      },
+      {
+      id: 6,
+      title: "How many participants can join a ClearLink video conference?",
+      content:"ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scalewebinars, ensuring you have the right fit for your needs."
+    },
+  ],
+};
---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { GoPlus } from "react-icons/go";
+import { FiMinus } from "react-icons/fi";
+function AccordionCard({ title, content}) {
+  //stae to handle open and closing of the accordion
+  const [isOpen, setIsOpen] = useState(false);
+  //function responsible for the opening and closing accordion
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <div className="w-full mx-auto px-5">
+        <div className="grid divide-y divide-neutral-200 max-w-xl mx-auto mt-8">
+          <div onClick={toggle} className="py-3 max-w-400px">
+            <details className="group">
+              <summary className=" border space-x-3 flex justify-between items-center font-medium cursor-pointer list-none">
+                <span>{title}</span>
+                <span className="transition group-open:rotate-180">
+                  {!isOpen ? <GoPlus /> : <FiMinus />}
+                </span>
+              </summary>
+              {isOpen && (
+                <p className="text-neutral-600 mt-3 group-open:animate-fadeIn">
+                  {content}
+                </p>
+              )}
+            </details>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AccordionCard;
